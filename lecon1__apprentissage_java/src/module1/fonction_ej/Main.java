26038e5f8e661223f378fafe16cb39d27c095234
Calcagno Loïc <98493858+CalcagnoLoic@users.noreply.github.com>
2025-01-20 23:53:03 +0100

    Réorganisation + ajout du readme

---
+++ lecon1__apprentissage_java/src/module1/fonction_ej/Main.java
@@ -0,0 +1,27 @@
+package fonction_ej;
+
+public class Main {
+    public static void main(String[] args){
+        String affiche = Main.affiche();
+        String affiche2 = Main.afficheV2("Afficher du texte depuis Main");
+        int somme = Main.addition(5, 8);
+
+        System.out.println(affiche);
+        System.out.println(affiche2);
+        System.out.println(somme);
+    }
+
+    public static String affiche(){
+        System.out.println("Une fonction en Java a été exécutée” dans la console");
+        return null;
+    }
+
+    public static String afficheV2(String texte) {
+        System.out.println(texte);
+        return null;
+    }
+
+    public static int addition(int number1, int number2) {
+        return number1 + number2;
+    }
+}
