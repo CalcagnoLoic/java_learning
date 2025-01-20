8516b9f02a65e9ff4b12061af53a98518e25f79d
Calcagno Loïc <98493858+CalcagnoLoic@users.noreply.github.com>
2025-01-20 10:41:17 +0100

    Ajout des exercices sur les variables

---
+++ apprentissage_java/src/variable_ej/Main.java
@@ -0,0 +1,25 @@
+package variable_ej;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args){
+        String langage = "Java";
+        String phrase = langage + " est un langage de programmation.";
+
+        System.out.println(phrase);
+
+        int[] unTableauDEntier = {5,10,15,20};
+
+        String[] unTableauDeChar = new String[3];
+        unTableauDeChar[0] = "a";
+        unTableauDeChar[1] = "n";
+        unTableauDeChar[2] = "z";
+
+        System.out.println(Arrays.toString(unTableauDeChar));
+        System.out.println(Arrays.toString(unTableauDEntier));
+
+        String variable = "Hello, World!";
+        System.out.println(variable);
+    }
+}
